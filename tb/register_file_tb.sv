d18274f25eca9fa06013cbd4edab65526d3a0a85
aayush.sinha <aayusharyaman@gmail.com>
2026-07-13 20:27:06 -0700

    to repo

---
+++ tb/register_file_tb.sv
@@ -0,0 +1,12 @@
+module register_file_tb();
+
+    logic clk; 
+    logic write_enable; 
+    logic [4:0] write_address; 
+    logic [4:0] read_address1;
+    logic [4:0] read_address2;
+    logic [31:0] write_data; 
+    logic [31:0] read_data1; 
+    logic [31:0] read_data2; 
+
+endmodule 
